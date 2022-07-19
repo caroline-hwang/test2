6a61e2857849feddd5323863e6ed6ac1f5b35ce3
carolinehwang13 <caroline_hwang@brown.edu>
2022-07-19 13:56:37 -0400

    fghkjl

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func button(_ sender: UIButton) {
+        
+        label.text = "new text"
+        
+    }
 
 }
 
